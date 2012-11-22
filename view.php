fe040a0df260e18008596d331b4e6b5db4ac0245
root <root@f3680020-5eea-4431-ad50-22e2b9497ebd.cs1b02cloud.internal>
2012-11-22 11:24:15 +0900

    add view.php

--- view.php
+++ view.php
@@ -11,12 +11,19 @@ mysql_select_db(DBNAME);
 
 $id = 0;
 if ( isset($_GET['id']) )
-    $pagenum = $_GET['id'];
+    $id = $_GET['id'];
 
 $items = array();
 $query = "select * from tbl_goods where id=$id ";
+
 $result = mysql_query($query);
 $count = mysql_num_rows($result);
+$item = mysql_fetch_array($result);
+
+$query = "select * from tbl_seller where id=".$item['seller'];
+$result = mysql_query($query);
+$count = mysql_num_rows($result);
+$seller = mysql_fetch_array($result);
 
 $page = array(
     'title' => "A Farmer's Marke",
@@ -24,18 +31,39 @@ $page = array(
     'login' => $_SESSION["login"],
     'name' => $_SESSION["name"],
     'item' => array(
-        'id' => 1,
-        'label' => '상품',
-        'title' => '밴쿠버 진천쌀',
-        'price' => 28000,
-        'comment' => "밴쿠버 최고의 농부가 생산한 햅쌀, 밥만 먹어도 밥이 뚝딱, 사라지는 밥도둑, 일단 한번 잡사봐. 한번 먹으면 잊을 수 없는 밥맛, 우리 애들이 달라졌어요. 엄마 밥줘!!! 맨날 햄버거, 피자를 찾던 아이들이, 밥을 찾습니다."
+        'id' => $item['id'],
+        'label' => $item['label'],
+        'title' => $item['title'],
+        'price' => $item['price'],
+        'comment' => $item['comment']
     ),
     'seller' => array(
-        'name' => '김정수',
-        'phone' => '000-000-0000',
-        'comment' => '밴쿠버 최고의 농부'
+        'name' => $seller['name'],
+        'phone' => $seller['phone'],
+        'comment' => $seller['comment']
     ),
-    'images' => array()
+    'images' => array(
+        array(
+            'starttag' => '<tr>',
+            'image' => 'http://img1.coupangcdn.com/image/dd/61/63/27936163_7b7104f9-298b-4b32-b452-d95c8430e83a.jpg',
+            'endtag' => ''
+        ),
+        array(
+            'starttag' => '',
+            'image' => 'http://img1.coupangcdn.com/image/dd/61/63/27936163_7b7104f9-298b-4b32-b452-d95c8430e83a.jpg',
+            'endtag' => ''
+        ),
+        array(
+            'starttag' => '',
+            'image' => 'http://img1.coupangcdn.com/image/dd/61/63/27936163_7b7104f9-298b-4b32-b452-d95c8430e83a.jpg',
+            'endtag' => '</tr>'
+        ),
+        array(
+            'starttag' => '<tr>',
+            'image' => 'http://img1.coupangcdn.com/image/dd/61/63/27936163_7b7104f9-298b-4b32-b452-d95c8430e83a.jpg',
+            'endtag' => '</tr>'
+        )
+    )
 );
 
 echo $h2o->render(compact('page'));
