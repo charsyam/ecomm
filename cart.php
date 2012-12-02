c64d0c3f7e86364e52702f54edd3874d221cd340
root <root@f3680020-5eea-4431-ad50-22e2b9497ebd.cs1b02cloud.internal>
2012-12-03 03:49:06 +0900

    add cart

--- cart.php
+++ cart.php
@@ -5,14 +5,29 @@ require 'util.php';
 require 'config.php';
 
 $h2o = new h2o('templates/cart.html');
+
+$count = -1;
+if( isset( $_GET["count"] ) ) {
+    $count = $_GET["count"];
+}
+
 if( isset( $_GET["id"] ) ) {
     $id = $_GET["id"];
-    $value = 1;
-    if( isset( $_SESSION["cart"]["$id"] ) ){
-        $value = $_SESSION["cart"]["$id"];
-        $value++;
+
+    if ( $count != 0 ){
+        $value = 1;
+        if( isset( $_SESSION["cart"]["$id"] ) ){
+            $value = $_SESSION["cart"]["$id"];
+            if( $count > 0 ){
+                $value = $count;
+            }else{
+                $value++;
+            }
+        }
+        $_SESSION["cart"]["$id"] = $value;
+    }else{
+        unset( $_SESSION["cart"]["$id"] );
     }
-    $_SESSION["cart"]["$id"] = $value;
 }
 
 $items = array();
@@ -34,40 +49,47 @@ foreach( $_SESSION["cart"] as $id => $count ){
 $link = mysql_connect( DBHOST, DBUSER, DBPASS ) or die('Could not connect: '.mysql_error());
 mysql_select_db(DBNAME);
 
-$query = "select * from tbl_goods where id in ($idlist)";
-$result = mysql_query( $query );
-$numcount = mysql_num_rows($result);
-$num = 0;
-while( $row = mysql_fetch_array($result) )
-{
-    $starttag = '';
-    $endtag = '';
+$total_price =0;
 
-    if( $num % 3 == 0){
-        $starttag = "<tr>";
+if( $idlist != "" ){
+    $query = "select * from tbl_goods";
+    $query = $query." where id in ($idlist)";
+    $result = mysql_query( $query );
+    $numcount = mysql_num_rows($result);
+    $num = 0;
+
+    while( $row = mysql_fetch_array($result) )
+    {
+        $starttag = '';
+        $endtag = '';
+
+        if( $num % 3 == 0){
+            $starttag = "<tr>";
+        }
+        if( ($num % 3) == 2 || $num == ($numcount-1)){
+            $endtag = "</tr>";
+        }
+
+        $id = $row['id'];
+        $count = $_SESSION["cart"]["$id"];
+        $price = $row['price'];
+        $realprice = $count * $price;
+        $newitem = array(
+                'starttag' => $starttag,
+                'endtag' => $endtag,
+                'id' => $row['id'],
+                'label' => '상품',
+                'title' => $row['title'],
+                'price' => $row['price'],
+                'image' => $row['image'],
+                'count' => $count,
+                'realprice' => $realprice
+                );
+
+        $total_price += $realprice;
+        array_push( $items, $newitem );
+        $num++;
     }
-    if( ($num % 3) == 2 || $num == ($numcount-1)){
-        $endtag = "</tr>";
-    }
-
-    $id = $row['id'];
-    $count = $_SESSION["cart"]["$id"];
-    $price = $row['price'];
-    $realprice = $count * $price;
-    $newitem = array(
-            'starttag' => $starttag,
-            'endtag' => $endtag,
-            'id' => $row['id'],
-            'label' => '상품',
-            'title' => $row['title'],
-            'price' => $row['price'],
-            'image' => $row['image'],
-            'count' => $count,
-            'realprice' => $realprice
-    );
-
-    array_push( $items, $newitem );
-    $num++;
 }
 
 $login = 0;
@@ -85,6 +107,7 @@ $page = array(
     'currenttime' => getCurrentTime(),
     'login' => $login,
     'name' => "$name",
+    'totalprice' => $total_price,
     'itemcount' => count($items),
     'items' => $items
 );
